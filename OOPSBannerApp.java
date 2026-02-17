de494831fa1fa6c912d7ac446a61cd1934022109
arpitaghatge28-collab <arpitaghatge28@gmail.com>
2026-02-17 09:08:38 +0530

    OPPSBannerApp has be edited in develope

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,15 @@
+
+/**
+ * OOPSBannerApp UC1 - OPPS Banner Display Application
+ * 
+ * This class demonstrates a simple Java application that displays the Object 
+ * Oriented Programming System OOPS acronym to the console.
+ * 
+ * @author Developer 
+ */
+
 public class OOPSBannerApp {
    public static void main(String[] args) {
-       System.out.println("OOPS Banner App Base");
+       System.out.println("OOPS");
    }
 }
