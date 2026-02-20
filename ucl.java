e39a2ba1208421aaaca7b6321de61f0578c80994
arpitaghatge28-collab <arpitaghatge28@gmail.com>
2026-02-20 11:08:26 +0530

    ucl edited

--- ucl.java
+++ ucl.java
@@ -1,4 +1,4 @@
-public class OOPSBannerApp  {
+public class ucl  {
     public static void main(String[] args) {
         System.out.println("Your banner here");
 System.out.println(String.join("   ***   " + " " + "   ***   "));
