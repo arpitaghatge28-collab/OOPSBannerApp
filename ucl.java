16078aa94cd4fc5ffbfa623e08c3b70837f10e22
arpitaghatge28-collab <arpitaghatge28@gmail.com>
2026-02-20 11:04:20 +0530

    ucl  edited

--- ucl.java
+++ ucl.java
@@ -1,12 +1,13 @@
-System.out.println("   ***   " + " " + "   ***   ");
-System.out.println(" **   ** " + " " + " **      ");
-System.out.println("**     **" + " " + "**       ");
-System.out.println("**     **" + " " + "  *****  ");
-System.out.println("**     **" + " " + "      ** ");
-System.out.println(" **   ** " + " " + " **   ** ");
-System.out.println("   ***   " + " " + "   ***** ");
 public class OOPSBannerApp  {
     public static void main(String[] args) {
+        System.out.println("Your banner here");
+System.out.println(String.join("   ***   " + " " + "   ***   "));
+System.out.println(String.join(" **   ** " + " " + " **      "));
+System.out.println(String.join("**     **" + " " + "**       "));
+System.out.println(String.join("**     **" + " " + "  *****  "));
+System.out.println(String.join("**     **" + " " + "      ** "));
+System.out.println(String.join(" **   ** " + " " + " **   ** "));
+System.out.println(String.join("   ***   " + " " + "   ***** "));
         
     }
 }
