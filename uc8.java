b301344429d646f7e280fbf14fb907a320435182
arpitaghatge28-collab <arpitaghatge28@gmail.com>
2026-03-18 09:37:37 +0530

    uc8

---
+++ uc8.java
@@ -0,0 +1,67 @@
+/**
+ * OOPSBannerApp UC6 - OOPS Banner Application (Use Case 6)
+ * * This use case extends UC5 by implementing a modular approach to generate each
+ * letter's pattern through dedicated methods. This enhances code reusability and
+ * maintainability by separating pattern generation logic from the main display logic.
+ * * @author Developer
+ * @version 6.0
+ */
+
+public class OOPSBannerApp {
+
+    // Method to generate the pattern for the letter 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+            "  *** ",
+            " * * ",
+            " * * ",
+            " * * ",
+            "  *** "
+        };
+    }
+
+    // Method to generate the pattern for the letter 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+            " ***** ",
+            " * *",
+            " ***** ",
+            " * ",
+            " * "
+        };
+    }
+
+    // Method to generate the pattern for the letter 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+            "  **** ",
+            " * ",
+            "  *** ",
+            "     * ",
+            " **** "
+        };
+    }
+
+    // Main method to run the banner display
+    public static void main(String[] args) {
+
+        // Declare String Arrays to hold patterns for each letter
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        // Use the loop to Assemble each line of the banner to create the 
+        // visual effect for the message "OOPS"
+        // We use oPattern.length assuming all letter arrays have the same height
+        for (int i = 0; i < oPattern.length; i++) {
+            // Join the parts for each line: O + O + P + S
+            String line = String.join("  ", 
+                oPattern[i], 
+                oPattern[i], 
+                pPattern[i], 
+                sPattern[i]
+            );
+            System.out.println(line);
+        }
+    }
+}
